b15b7956cc8b24db86d3edc67b8a6af8dea36729
computerphobe <aryanbharadiyak2006@gmail.com>
2025-12-03 22:04:03 +0530

    removed webpacks

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ef76aaffc9c8a0a8"; }
+/******/ 		__webpack_require__.h = function() { return "fae247cff8e2b80d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.9d0bae9f27a4132c.hot-update.js
+++ .next/static/webpack/webpack.9d0bae9f27a4132c.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "ef76aaffc9c8a0a8"; }
+/******/ 	__webpack_require__.h = function() { return "fae247cff8e2b80d"; }
 /******/ }();
 /******/ 
 /******/ }
